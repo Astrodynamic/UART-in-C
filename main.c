fd0d55f9712fe38d092449789719072e4bf83bbf
Astrodynamic <Astrodynamic.fkg@gmail.com>
2023-04-14 22:04:44 +0700

    finish

--- main.c
+++ main.c
@@ -1,174 +1,136 @@
+#include <fcntl.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
-#include <termios.h>
-#include <fcntl.h>
+#include <sys/msg.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-#define BUF_SIZE 256
+#define PACKET_SIZE 7  // Размер пакета
+#define SYNCBYTE 0x25  // Значение синхробайта
 
-int fd1, fd2;                        // Дескрипторы файлов для COM портов
-pthread_mutex_t mutex;               // Мьютекс для синхронизации доступа к COM портам
-pthread_cond_t cond1, cond2;         // Условные переменные для ожидания появления данных в COM портах
-pthread_t thread1, thread2;          // Идентификаторы потоков
-int port1, port2;                    // Номера COM портов
-char buf1[BUF_SIZE], buf2[BUF_SIZE]; // Буферы для чтения/записи данных из/в COM порты
+struct msg_buffer {
+  long mtype;
+  char mtext[PACKET_SIZE];
+};
 
-/* Функция-обработчик первого потока */
-void* thread_func1(void* arg __attribute__((unused))) {
-    while (1) {
-        // Ждем сигнала от основного потока о готовности к работе
-        pthread_mutex_lock(&mutex);
-        pthread_cond_wait(&cond1, &mutex);
-        pthread_mutex_unlock(&mutex);
+int fd_com_1, fd_com_2;  // Дескрипторы файлов для com-портов
+int sys_msg_id;  // Идентификатор системной очереди
 
-        // Считываем данные из COM порта 1
-        int nread = read(fd1, buf1, BUF_SIZE - 1);
-        if (nread > 0) {
-            buf1[nread] = '\0';
-            printf("Received from port %d: %s\n", port1, buf1);
-
-            // Инвертируем байты данных
-            for (int i = 0; i < nread; i += 2) {
-                char tmp = buf1[i];
-                buf1[i] = buf1[i + 1];
-                buf1[i + 1] = tmp;
-            }
-
-            // Отправляем данные в COM порт 2
-            write(fd2, buf1, nread);
-        } else if (nread == -1) {
-            perror("read");
-        }
-    }
-}
-
-/* Функция-обработчик второго потока */
-void* thread_func2(void* arg __attribute__((unused))) {
-    while (1) {
-        // Ждем сигнала от основного потока о готовности к работе
-        pthread_mutex_lock(&mutex);
-        pthread_cond_wait(&cond2, &mutex);
-        pthread_mutex_unlock(&mutex);
-
-        // Считываем данные из COM порта 2
-        int nread = read(fd2, buf2, BUF_SIZE - 1);
-        if (nread > 0) {
-            buf2[nread] = '\0';
-            printf("Received from port %d: %s\n", port2, buf2);
-        } else if (nread == -1) {
-            perror("read");
-        }
-    }
-}
-
-/* Функция для настройки параметров COM порта */
-void set_serial_port(int fd, int speed) {
-    struct termios tty;
-    memset(&tty, 0, sizeof tty);
-
-    if (tcgetattr(fd, &tty) != 0) {
-        perror("tcgetattr");
-        return;
-    }
-
-    cfsetospeed(&tty, speed);
-    cfsetispeed(&tty, speed);
-
-    // Устанавливаем параметры порта
-    tty.c_cflag &= ~PARENB;         // Без проверки четности
-    tty.c_cflag &= ~CSTOPB;         // Один стоп-бит
-    tty.c_cflag &= ~CSIZE;          // Маска битов данных
-    tty.c_cflag |= CS8;             // 8 бит данных
-    tty.c_cc[VMIN]  = 1;            // Минимальное количество символов для чтения
-    tty.c_cc[VTIME] = 5;            // Таймаут ожидания ввода/вывода
-
-    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-        perror("tcsetattr");
-        return;
-    }
-}
-
-/* Функция для инициализации COM порта */
-int init_serial_port(const char* port_name) {
-    int fd = open(port_name, O_RDWR | O_NOCTTY | O_SYNC);
-    if (fd == -1) {
-        perror("open");
-        return -1;
-    }
-
-    set_serial_port(fd, B115200);
-
-    return fd;
-}
+void *thread1_handler(void *arg);
+void *thread2_handler(void *arg);
 
 int main() {
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&cond1, NULL);
-    pthread_cond_init(&cond2, NULL);
+  int com_1, com_2;
+  pthread_t thread_1, thread_2;
 
-    // Получаем номера COM портов от пользователя
-    printf("Enter port number for stream 1 (1-99): ");
-    scanf("%d", &port1);
+  // Получаем номер порта для потока No1 от пользователя
+  printf("Введите номер COM для ПОТОК No1 число от 1-99: ");
+  scanf("%d", &com_1);
+  char device_1[20];
+  sprintf(device_1, "/dev/USBtty%d", com_1);
 
-    printf("Enter port number for stream 2 (1-99): ");
-    scanf("%d", &port2);
+  // Получаем номер порта для потока No2 от пользователя
+  printf("Введите номер COM для ПОТОК No2 число от 1-99: ");
+  scanf("%d", &com_2);
+  char device_2[20];
+  sprintf(device_2, "/dev/USBtty%d", com_2);
 
-    // Инициализируем COM порты
-    char port_name1[16], port_name2[16];
-    sprintf(port_name1, "/dev/ttyUSB%d", port1);
-    sprintf(port_name2, "/dev/ttyUSB%d", port2);
+  // Открываем файлы для com-портов
+  fd_com_1 = open(device_1, O_RDWR | O_NOCTTY | O_NONBLOCK);
+  fd_com_2 = open(device_2, O_RDWR | O_NOCTTY | O_NONBLOCK);
 
-    fd1 = init_serial_port(port_name1);
-    if (fd1 == -1) {
-        exit(EXIT_FAILURE);
+  // Создаем системную очередь
+  sys_msg_id = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
+  if (sys_msg_id < 0) {
+    perror("Error creating system message queue");
+    exit(1);
+  }
+
+  // Запускаем потоки
+  pthread_create(&thread_1, NULL, thread1_handler, NULL);
+  pthread_create(&thread_2, NULL, thread2_handler, NULL);
+
+  printf("START\n");
+
+  // Ждем завершения потоков
+  pthread_join(thread_1, NULL);
+  pthread_join(thread_2, NULL);
+
+  return 0;
+}
+
+void *thread1_handler(void *arg) {
+  int read_bytes;
+  char buffer[PACKET_SIZE];
+  struct msg_buffer msg;
+
+  while (1) {
+    // Получаем пакет от пользователя через ком-порт 1
+    printf("Insert_TX_Packet:\n");
+    read_bytes = read(fd_com_1, buffer, PACKET_SIZE);
+    if (read_bytes != PACKET_SIZE) {
+      printf("Error_TX_Packet\n");
+      continue;
     }
 
-    fd2 = init_serial_port(port_name2);
-    if (fd2 == -1) {
-        close(fd1);
-        exit(EXIT_FAILURE);
+    // Проверяем синхробайты
+    if (buffer[0] != SYNCBYTE || buffer[1] != SYNCBYTE ||
+        buffer[2] != SYNCBYTE || buffer[3] != SYNCBYTE) {
+      printf("Error_TX_Packet\n");
+      continue;
     }
 
-    // Создаем потоки
-    int rc = pthread_create(&thread1, NULL, thread_func1, NULL);
-    if (rc != 0) {
-        perror("pthread_create");
-        close(fd1);
-        close(fd2);
-        exit(EXIT_FAILURE);
+    // Получаем длину пакета и полезную информацию
+    int packet_len = (int)buffer[4];
+    if (packet_len != PACKET_SIZE - 5) {
+      printf("Error_TX_Packet\n");
+      continue;
+    }
+    memcpy(msg.mtext, buffer + 4, packet_len + 1);
+
+    // Отправляем сообщение в системную очередь
+    msg.mtype = 1;
+    if (msgsnd(sys_msg_id, &msg, sizeof(msg), 0) < 0) {
+      perror("Error sending message to system queue");
+      exit(1);
+    }
+  }
+}
+
+void *thread2_handler(void *arg) {
+  int write_bytes;
+  char buffer[PACKET_SIZE];
+  struct msg_buffer msg;
+
+  while (1) {
+    // Получаем сообщение из системной очереди
+    if (msgrcv(sys_msg_id, &msg, sizeof(msg.mtext), 1, 0) < 0) {
+      perror("Error receiving message from system queue");
+      exit(1);
     }
 
-    rc = pthread_create(&thread2, NULL, thread_func2, NULL);
-    if (rc != 0) {
-        perror("pthread_create");
-        close(fd1);
-        close(fd2);
-        exit(EXIT_FAILURE);
+    buffer[0] = msg.mtext[4];
+
+    // Инвертируем полезную информацию
+    for (int i = 0; i < PACKET_SIZE - 5; ++i) {
+      buffer[i + 1] = msg.mtext[PACKET_SIZE - 6 - i];
+    }
+    
+    // Отправляем пакет через ком-порт 2
+    write_bytes = write(fd_com_2, buffer, buffer[0] + 1);
+    if (write_bytes != buffer[0] + 1) {
+      perror("Error writing packet to com2 port");
+      exit(1);
     }
 
-    // Основной цикл программы
-    while (1) {
-        // Выводим приглашение к вводу сообщения для передачи
-        printf("Insert TX packet: ");
-        fflush(stdout);
-
-        // Считываем строку из консоли
-        char input_buf[BUF_SIZE];
-        fgets(input_buf, BUF_SIZE, stdin);
-
-        // Отправляем данные в COM порт 1
-        pthread_mutex_lock(&mutex);
-        strcpy(buf1, input_buf);
-        write(fd1, buf1, strlen(input_buf));
-        pthread_cond_signal(&cond1);
-        pthread_cond_signal(&cond2);
-        pthread_mutex_unlock(&mutex);
+    // Выводим принятый пакет в терминал
+    printf("ReceivePacket: ");
+    for (int i = 0; i < PACKET_SIZE; ++i) {
+      printf("[%X]", buffer[i]);
     }
-
-    // Не дойдем до сюда, программа завершится по сигналу
-    close(fd1);
-    close(fd2);
-    exit(EXIT_SUCCESS);
+    printf("\n");
+  }
 }
